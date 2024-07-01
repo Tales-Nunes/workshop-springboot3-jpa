65f52bba7fa0ee9b5b5646c10a6e611367915ffc
Tales Nunes <talesnunes.siqueira@gmail.com>
2024-07-01 19:26:39 -0300

    Project open

---
+++ src/main/java/com/projeto/webServicesCourse/WebServicesCourseApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.webServicesCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesCourseApplication.class, args);
+	}
+
+}
