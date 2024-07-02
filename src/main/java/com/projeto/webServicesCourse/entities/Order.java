7de1a6b8f8cee87f2dc22cd1c73846f710c15fd8
Tales Nunes <talesnunes.siqueira@gmail.com>
2024-07-02 11:10:36 -0300

    OrderStatus enum

--- src/main/java/com/projeto/webServicesCourse/entities/Order.java
+++ src/main/java/com/projeto/webServicesCourse/entities/Order.java
@@ -1,6 +1,7 @@
 package com.projeto.webServicesCourse.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.projeto.webServicesCourse.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,6 +17,7 @@ public class Order implements Serializable {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+    private Integer orderStatus;
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -23,9 +25,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -43,6 +46,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
