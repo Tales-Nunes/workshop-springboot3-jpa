674327ed9beac254ba39780d4b0ed8ac8e02b4e0
Tales Nunes <talesnunes.siqueira@gmail.com>
2024-07-01 21:16:14 -0300

    H2 database, test profile, JPA

--- src/main/java/com/projeto/webServicesCourse/entities/User.java
+++ src/main/java/com/projeto/webServicesCourse/entities/User.java
@@ -1,10 +1,15 @@
 package com.projeto.webServicesCourse.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
-
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
