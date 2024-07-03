7a1fb35a17fe23efc9b47d0cc2ed14579026c17a
Tales Nunes <talesnunes.siqueira@gmail.com>
2024-07-03 19:35:24 -0300

    User insert

--- src/main/java/com/projeto/webServicesCourse/services/UserService.java
+++ src/main/java/com/projeto/webServicesCourse/services/UserService.java
@@ -20,6 +20,9 @@ public class UserService {
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
         return obj.get();
+    }
 
+    public User insert(User obj) {
+        return repository.save(obj);
     }
 }
