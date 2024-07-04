614193fb9c4b7bf79f731c3df15d32f5ea086742
Tales Nunes <talesnunes.siqueira@gmail.com>
2024-07-04 14:12:17 -0300

    Exception handling - update

--- src/main/java/com/projeto/webServicesCourse/services/UserService.java
+++ src/main/java/com/projeto/webServicesCourse/services/UserService.java
@@ -4,6 +4,7 @@ import com.projeto.webServicesCourse.entities.User;
 import com.projeto.webServicesCourse.repositories.UserRepository;
 import com.projeto.webServicesCourse.services.exceptions.DataBaseException;
 import com.projeto.webServicesCourse.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -41,9 +42,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try{
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch(EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
 
     }
 
